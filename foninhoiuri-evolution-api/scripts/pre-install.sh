e4ec7f4fb5d3a64079fc0f2b2347a324d3b11177
Igor Iuri <neon10.00086@gmail.com>
2025-05-29 11:14:29 -0300

    Delete foninhoiuri-evolution-api/scripts directory

--- foninhoiuri-evolution-api/scripts/pre-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "$AUTHENTICATION_API_KEY" ]; then
-  echo "ERRO: A variável AUTHENTICATION_API_KEY não foi fornecida pelo usuário."
-  exit 1
-fi
-
-echo "Chave de autenticação recebida com sucesso."

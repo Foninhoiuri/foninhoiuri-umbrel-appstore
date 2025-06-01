a10fa97e9589d50930732b9315a6b886dbbcc274
Igor Iuri <neon10.00086@gmail.com>
2025-05-31 23:37:31 -0300

    Create export.sh

---
+++ foninhoiuri-postgresql/export.sh
@@ -0,0 +1,9 @@
+# Este arquivo define as variáveis de ambiente sensíveis para o seu aplicativo.
+# É uma boa prática não commitar este arquivo em repositórios públicos.
+
+export POSTGRES_USER="seu_usuario_seguro" # Altere para um usuário forte
+export POSTGRES_PASSWORD="sua_senha_muito_segura" # Altere para uma senha forte
+export POSTGRES_DB="seu_banco_de_dados" # Altere para o nome do seu banco de dados
+export TZ="America/Sao_Paulo" # Fuso horário, ajuste se necessário
+
+echo "Variáveis de ambiente carregadas!"

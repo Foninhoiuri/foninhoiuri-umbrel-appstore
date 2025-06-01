31034d1fa9541f5e93c3ec38fa032ed30d984e6f
Igor Iuri <neon10.00086@gmail.com>
2025-05-31 23:51:00 -0300

    Update and rename export.sh to credentials.sh

--- foninhoiuri-postgresql/credentials.sh
+++ foninhoiuri-postgresql/credentials.sh
@@ -1,9 +1,13 @@
 # Este arquivo define as variáveis de ambiente sensíveis para o seu aplicativo.
 # É uma boa prática não commitar este arquivo em repositórios públicos.
 
+
+# usado no DATABASE_CONNECTION_URI do Evolution API entre outros
 export POSTGRES_USER="seu_usuario_seguro" # Altere para um usuário forte
 export POSTGRES_PASSWORD="sua_senha_muito_segura" # Altere para uma senha forte
 export POSTGRES_DB="seu_banco_de_dados" # Altere para o nome do seu banco de dados
+
+
 export TZ="America/Sao_Paulo" # Fuso horário, ajuste se necessário
 
 echo "Variáveis de ambiente carregadas!"

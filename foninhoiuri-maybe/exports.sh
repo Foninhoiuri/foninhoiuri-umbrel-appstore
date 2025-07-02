515cb479248d6e2e7643f91cfa036ba61a5eebf8
Igor Iuri <neon10.00086@gmail.com>
2025-07-02 09:03:57 -0300

    Delete foninhoiuri-maybe/exports.sh

--- foninhoiuri-maybe/exports.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Chave secreta do app Maybe (gere com `openssl rand -hex 64`)
-export SECRET_KEY_BASE="REPLACE_THIS_WITH_YOUR_GENERATED_KEY"
-
-# Senha do Postgres
-export POSTGRES_PASSWORD="changeme123"
